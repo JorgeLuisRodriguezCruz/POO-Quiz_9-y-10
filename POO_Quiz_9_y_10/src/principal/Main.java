ae63d6332e7c71e6852cf08ce5309ef97698b61f
JoseA27 <josrobles@estudiantec.cr>
2022-11-04 11:37:50 -0600

    Main y Procesadores

--- POO_Quiz_9_y_10/src/principal/Main.java
+++ POO_Quiz_9_y_10/src/principal/Main.java
@@ -1,5 +1,11 @@
 package principal;
 
 public class Main {
+	public void main(String args[]) {
+		Procesador descripcion = new Descripcion_Video();
+		Algoritmo uno = new Algoritmo_Uno("a");
+		descripcion.setAlgoritmo(null);
+		descripcion.estado();
+	}
 
 }
--- POO_Quiz_9_y_10/src/principal/Procesador.java
+++ POO_Quiz_9_y_10/src/principal/Procesador.java
@@ -3,11 +3,12 @@ package principal;
 public abstract class Procesador {
 	private Algoritmo algoritmo;
 	
+	public Procesador() {
+		
+	}
 	protected abstract void estado();
 
 	public void setAlgoritmo(Algoritmo algoritmo) {
 		this.algoritmo = algoritmo;
 	}
-	
-	
 }
